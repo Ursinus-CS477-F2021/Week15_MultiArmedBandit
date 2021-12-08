1d7e637d9ea31a8d8a1aed3669714a331eec1ac3
Chris Tralie <chris.tralie@gmail.com>
2021-12-08 13:23:53 -0500

    Adding temporal changes

--- bandit.py
+++ bandit.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Bandit:
-    def __init__(self, K, seed):
+    def __init__(self, K, seed, change_lambda = 0):
         """
         Initialize a K-arm bandit
 
@@ -11,11 +11,14 @@ class Bandit:
             Number of arms
         seed: int
             Seed for repeatability 
+        change_lambda: float
+            Probability that a mean will change at any given time
         """
         self.K = K
         np.random.seed(seed)
         self.sigmas = np.abs(np.random.randn(K))
         self.mus = np.random.randn(K)
+        self.change_lambda = change_lambda
 
     def pull(self, k):
         """
@@ -26,7 +29,11 @@ class Bandit:
         k: int
             Arm index
         """
-        return self.mus[k] + self.sigmas[k]*np.random.randn()
+        res = self.mus[k] + self.sigmas[k]*np.random.randn()
+        for i in range(self.K):
+            if np.random.rand() < self.change_lambda:
+                self.mus[i] = np.random.randn() # Change this bandit
+        return res
     
 
 def naive_maximize_reward(bandit, n_trials):
